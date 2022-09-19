fdc0d9af135c13b8830be4d35e622410721cd154
dchay <dylanhay@rogers.com>
2022-09-19 13:12:28 -0700

    port updated

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const { notes } = require('./data/notes');
 
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 //may not be needed
@@ -24,6 +26,6 @@ app.get('/api/notes', (req, res) => {
   res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
