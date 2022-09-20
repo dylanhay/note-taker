2f31bfa714abd635156b70c763b492e55eb63eb6
dchay <dylanhay@rogers.com>
2022-09-20 14:32:18 -0700

    apiRoutes and htmlRoutes modularized

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 //port declaration
 const PORT = process.env.PORT || 3001;
 
@@ -16,105 +19,8 @@ app.use(express.json());
 //make public folder files available to server
 app.use(express.static('public'));
 
-//note functions
-
-function filterByQuery(query, notesArray) {
-  let filteredResults = notesArray;
-  if (query.title) {
-    filteredResults = filteredResults.filter(note => note.title === query.title);
-  }
-  if (query.text) {
-    filteredResults = filteredResults.filter(note => note.text === query.text);
-  }
-  return filteredResults;
-}
-
-function findById(id, notesArray) {
-  const result = notesArray.filter(note => note.id === id)[0];
-  return result;
-}
-
-function createNewNote(body, notesArray) {
-  const note = body;
-  notesArray.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, './data/notes.json'),
-    JSON.stringify({ notes: notesArray }, null, 2)
-  );
-  return note;
-}
-
-function deleteNote(id, notesArray) {
-  notesArray.splice(id, 1);
-  fs.writeFileSync(
-    path.join(__dirname, './data/notes.json'),
-    JSON.stringify({ notes: notesArray }, null, 2)
-  );
-  return id;
-}
-
-function validateNote(note) {
-  if (!note.title || typeof note.title !== 'string') {
-    return false;
-  }
-  if (!note.text || typeof note.text !== 'string') {
-    return false;
-  }
-  return true;
-}
-
-//api routes
-
-app.get('/api/notes', (req, res) => {
-  let results = notes;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
-});
-
-app.get('/api/notes/:id', (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
-
-app.post('/api/notes', (req, res) => {
-  req.body.id = notes.length.toString();
-
-  if (!validateNote(req.body)) {
-    res.status(400).send('The note is not properly formatted.');
-  } else {
-    const note = createNewNote(req.body, notes);
-    res.json(note);
-  }
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-  const result = deleteNote(req.params.id, notes);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
-
-//html routes
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
